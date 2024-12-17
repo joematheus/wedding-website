2edd483bbcc5697df4d45cbded4654a818eb2951
Theus <theusbusa@gmail.com>
2024-12-17 19:36:00 +0800

    initial commit

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  basePath: "/wedding-website",
+  output: "export",
+  reactStrictMode: true,
+};
+
+export default nextConfig;
