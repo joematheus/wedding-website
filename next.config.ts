fb7b549f8a5ed1b07b99bbf5ac709824a220e814
Theus <theusbusa@gmail.com>
2025-07-28 00:56:50 +0800

    added assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/wedding-website",
+  assetPrefix: "/wedding-website/",
   output: "export",
   reactStrictMode: true,
   images: {
